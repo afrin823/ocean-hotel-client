75e89cb93c0b4942625fb140bcc75f67818ea7c8
afrin823 <afrin12490@gmail.com>
2024-04-18 15:46:31 +0600

    login form

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-2xl">Home page</h2>
+        </div>
+    );
+};
+
+export default Home;
