7bc03851dc84a1acb3e02b5b81cdba971adf6302
afrin823 <afrin12490@gmail.com>
2024-04-21 21:49:48 +0600

    react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import './App.css';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import app from './firebase/firebase.config';
 
 
-const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider();
+
 function App() {
 
-  const handelGoogleSignIn = () => {
-    signInWithPopup(auth, googleProvider)
-    .then(result => {
-      const googleUser = result.user;
-      console.log(googleUser);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  }
+ 
 
   return (
     <>
-      <div>
-   
-     <h1>Assignment 09</h1>
-      
-       <div>
-        <button onClick={handelGoogleSignIn}>Google SignIn</button>
-       </div>
-      </div>
-      
+     
     </>
   )
 }
